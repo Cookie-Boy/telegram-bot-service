735bb88d08219f9de0abc059e52faa6e0702d4f7
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-05-20 14:31:53 +0700

    Fix SecurityConfig

--- src/main/java/ru/sibsutis/bot/configuration/SecurityConfig.java
+++ src/main/java/ru/sibsutis/bot/configuration/SecurityConfig.java
@@ -16,7 +16,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/health/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt())
